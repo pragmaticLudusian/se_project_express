7b742575b7a13045ec3307992f56be2e0aee03c1
pragmaticLudusian <arsen.r.m@gmail.com>
2025-03-25 21:31:52 +0200

    feat: return proper error for not finding resources

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -3,18 +3,18 @@ const ClothingItem = require("../models/clothingItem");
 module.exports.getItems = (req, res) => {
   ClothingItem.find({})
     .then((items) => res.send({ data: items }))
-    .catch((err) => res.status(500).send({ message: err }));
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 module.exports.createItem = (req, res) => {
   const { name, weather, imageUrl } = req.body;
   ClothingItem.create({ name, weather, imageUrl })
     .then((item) => res.send({ data: item }))
-    .catch((err) => res.status(500).send({ message: err }));
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 module.exports.deleteItem = (req, res) => {
   ClothingItem.findByIdAndDelete(req.params.id)
     .then((item) => res.send({ data: item }))
-    .catch((err) => res.status(500).send({ message: err }));
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
--- controllers/users.js
+++ controllers/users.js
@@ -3,18 +3,25 @@ const User = require("../models/user");
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: err }));
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params.id)
+    .orFail(() => {
+      const error = new Error("Requested resource not found");
+      error.statusCode = 404;
+      throw error;
+    }) // doesn't cover ObjectID casting (24-char string)
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err }));
+    .catch(
+      (err) => res.status(err.statusCode || 500).send({ message: err.message }) // return 404 or otherwise the generic 500
+    );
 };
 
 module.exports.createUser = (req, res) => {
   const { name, avatar } = req.body;
   User.create({ name, avatar })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err }));
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
